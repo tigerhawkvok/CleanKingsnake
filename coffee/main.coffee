a817983501168757e2f87ed0c10ed8b8907fb2a8
Philip Kahn <tigerhawkvok@gmail.com>
2014-12-28 15:42:59 -0800

    Fully functional cleanup

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -17,6 +17,32 @@ removeAds = ->
 
 
 prettify = ->
+  $("#container > table center + link + p + p + p + div + div").attr("id","main-listing-container")
+  $("#main-listing-container > center > table").attr("id","main-listing")
+  $("#main-listing-container").attr("style","")
+  $("#main-listing td").removeAttr("bgcolor")
+  $("#main-listing tr").removeAttr("style")
+  $("#main-listing tbody tr td[colspan]").each ->
+    $(this).parent().remove()
+  false
+
+helpers = ->
+  $("#main-listing td img").each ->
+    # Link it to the real image
+    href = $(this).parent().attr("href")
+    $(this).parent().removeAttr("href")
+    src = $(this).attr("src")
+    path = src.split("/")
+    img = path.pop()
+    newPath = "images/#{img}"
+    # Lightbox this image with a new link to the listing
+    $(this).parent().click ->
+      # Do lightbox
+      html = "<div class='extension-lightbox'><img src='#{newPath}' alt=''/><br/><p><a href='#{href}'>Go to listing &#187;</a></div>"
+      $(this).after(html)
+      $(".extension-lightbox img").click ->
+        $(".extension-lightbox").remove()
+    $(this).attr("title","Click to see listing picture")
   false
 
 $ ->
--- extension/c.js
+++ extension/c.js
@@ -1,4 +1,4 @@
-var prettify, removeAds;
+var helpers, prettify, removeAds;
 
 removeAds = function() {
   $(".splashText").remove();
@@ -12,6 +12,36 @@ removeAds = function() {
 };
 
 prettify = function() {
+  $("#container > table center + link + p + p + p + div + div").attr("id", "main-listing-container");
+  $("#main-listing-container > center > table").attr("id", "main-listing");
+  $("#main-listing-container").attr("style", "");
+  $("#main-listing td").removeAttr("bgcolor");
+  $("#main-listing tr").removeAttr("style");
+  $("#main-listing tbody tr td[colspan]").each(function() {
+    return $(this).parent().remove();
+  });
+  return false;
+};
+
+helpers = function() {
+  $("#main-listing td img").each(function() {
+    var href, img, newPath, path, src;
+    href = $(this).parent().attr("href");
+    $(this).parent().removeAttr("href");
+    src = $(this).attr("src");
+    path = src.split("/");
+    img = path.pop();
+    newPath = "images/" + img;
+    $(this).parent().click(function() {
+      var html;
+      html = "<div class='extension-lightbox'><img src='" + newPath + "' alt=''/><br/><p><a href='" + href + "'>Go to listing &#187;</a></div>";
+      $(this).after(html);
+      return $(".extension-lightbox img").click(function() {
+        return $(".extension-lightbox").remove();
+      });
+    });
+    return $(this).attr("title", "Click to see listing picture");
+  });
   return false;
 };
 
